754ccda2a282814ce86b29b4075d75010159c4a0
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-07-27 16:31:31 +0200

    feat: add manufacturer portal pages

---
+++ src/app/manufacturer/settings/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { RoleSettingsPage } from "@/components/app/modules/SettingsPage";
+
+export default function ManufacturerSettings() {
+  return <RoleSettingsPage role="manufacturer" />;
+}
